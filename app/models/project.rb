258de4bcf0e8f2ea72ae42e2d17d6d9e441e50ae
Greg Conen <gregconen@gmail.com>
2015-06-10 18:10:41 -0700

    validate positive goal amounts

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
   validates :owner, :title, :description, :goal_amount, presence: true
   validates :title, uniqueness: {scope: :owner}
+  validates :goal_amount, numericality: { greater_than: 0 }
   belongs_to(
     :owner,
     class_name: "User",
