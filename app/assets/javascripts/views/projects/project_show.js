5cf20f1b9c73469200c32f99fdfb3f127a730142
Greg Conen <gregconen@gmail.com>
2015-06-12 12:02:54 -0700

    add extremely ugly display of rewards to project show page

--- app/assets/javascripts/views/projects/project_show.js
+++ app/assets/javascripts/views/projects/project_show.js
@@ -6,6 +6,20 @@ Kickstarter.Views.ProjectShow = Backbone.CompositeView.extend({
     this.addSubview(".progress", this.progressView());
   },
 
+  addRewardView: function (reward) {
+    var rewardView = new Kickstarter.Views.RewardShow({
+      model: reward,
+    });
+    this.addSubview(".rewards-list", rewardView);
+  },
+
+  addRewardViews: function () {
+    this.removeSubviews(".reward-list");
+    this.model.rewards().each( function (reward) {
+      this.addRewardView(reward);
+    }.bind(this));
+  },
+
   progressView: function () {
     if (!this._progressView) {
       this._progressView = new Kickstarter.Views.ProjectProgress({
@@ -19,6 +33,7 @@ Kickstarter.Views.ProjectShow = Backbone.CompositeView.extend({
   render: function () {
     this.$el.html(this.template({ project: this.model }));
     this.attachSubviews();
+    this.addRewardViews();
 
     return this;
   }
---
+++ app/assets/javascripts/views/rewards/reward-show.js
@@ -0,0 +1,14 @@
+Kickstarter.Views.RewardShow = Backbone.View.extend({
+  template: JST['rewards/show'],
+
+  className:"rewards-list-item",
+
+
+  render: function () {
+    this.$el.html(this.template({
+      reward: this.model,
+      }));
+
+    return this;
+  },
+});
