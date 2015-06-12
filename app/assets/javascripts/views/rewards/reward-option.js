02154f6172e92dd7ade304670c8dd56a59a3fdbb
Greg Conen <gregconen@gmail.com>
2015-06-12 16:03:12 -0700

    have the pledge form autopopulate if a reward id is included in a query string

--- app/assets/javascripts/views/rewards/reward-option.js
+++ app/assets/javascripts/views/rewards/reward-option.js
@@ -4,10 +4,15 @@ Kickstarter.Views.RewardOption = Backbone.View.extend({
   tagName: "li",
   className:"rewards-options-list-item",
 
+  initialize: function (options) {
+    this.checked = options.checked;
+  },
+
 
   render: function () {
       this.$el.html(this.template({
-      reward: this.model,
+        reward: this.model,
+        checked: this.checked
       }));
 
     return this;
