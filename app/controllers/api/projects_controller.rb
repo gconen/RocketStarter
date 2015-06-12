556c77ae458e99141de6afcca63cca4beb05363f
Greg Conen <gregconen@gmail.com>
2015-06-12 11:29:37 -0700

    allow creation of rewards together with project

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -1,17 +1,17 @@
 module Api
   class ProjectsController < ApiController
     def index
-      @projects = Project.all
+      @projects = Project.all.includes(:owner)
       render :index
     end
 
     def show
-      @project = Project.includes(:owner).find(params[:id])
+      @project = Project.includes(:owner, :rewards).find(params[:id])
       render :show
     end
 
     def create
-      @project = current_user.projects.new(project_params)
+      @project = current_user.owned_projects.new(project_params)
       if @project.save
         render json: @project
       else
@@ -23,12 +23,22 @@ module Api
 
     def project_params
       project_params = params.require(:project)
-                            .permit(:id, :title, :description, :goal_amount)
+                .permit(:id, :title, :description, :goal_amount)
       project_params[:goal_amount] = project_params[:goal_amount]
                                         .gsub(/[\$,]/, "")
                                         .to_i
 
+      project_params[:rewards_attributes] = rewards_params
       project_params
+    end
+
+    def rewards_params
+      rewards_params = params.require(:project).require(:rewards)
+      rewards_params.map do |key, value|
+        reward = value.permit(:description, :amount)
+        reward[:amount] = reward[:amount].gsub(/[\$,]/, "").to_i
+        reward
+      end
     end
   end
 end
