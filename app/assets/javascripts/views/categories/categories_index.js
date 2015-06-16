8f42ad41023ce7c67d5f7e60c287dd68354b7fc3
Greg Conen <gregconen@gmail.com>
2015-06-15 17:57:09 -0700

    add basic project index view

--- app/assets/javascripts/views/categories/categories_index.js
+++ app/assets/javascripts/views/categories/categories_index.js
@@ -1,6 +1,27 @@
-Kickstarter.Views.CategoriesIndex = Backbone.View.extend({
+Kickstarter.Views.CategoriesIndex = Backbone.CompositeView.extend({
 
   template: JST['categories/index'],
 
+  initialize: function () {
+    this.listenTo(this.collection, "add", this.addCategory);
+  },
+
+  addCategories: function () {
+    this.collection.each( function (category) {
+      this.addCategory(category);
+    }.bind(this));
+  },
+
+  addCategory: function (category) {
+    var view = new Kickstarter.Views.CategoriesIndexItem({ model: category });
+    this.addSubview(".categories-list", view);
+  },
+
+  render: function () {
+    this.$el.html(this.template());
+    this.addCategories();
+
+    return this;
+  }
 
 });
